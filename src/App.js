b90f2a3dadcbb316775e5809cfc47d43ee2ac867
Тезяков Никита Андреевич <natezyakov@edu.hse.ru>
2020-01-20 18:20:22 +0500

    No validation and styling

--- src/App.js
+++ src/App.js
@@ -3,58 +3,62 @@ import axios from 'axios';
 import { formatDistance } from 'date-fns';
 import { Form, Field } from 'react-final-form';
 
-import './App.css';
+import styles from './styles.module.css';
 
 
 const App = () => {
   const [regDate, setRegDate] = React.useState(0);
-  const [dateDiff, setDateDiff] = React.useState(0);
+  const [dateDiff, setDateDiff] = React.useState(null);
+
+  React.useEffect(() => {
+    if (regDate) {
+      const dateDifference = formatDistance(
+        new Date(),
+        new Date(regDate),
+      );
+      setDateDiff(dateDifference)
+    }
+  }, [regDate]);
 
   const getRegDate = async login => {
-    try {
-      const data = await axios.get(`https://api.github.com/users/${login}`);
-      const date = data.data.created_at;
-      setRegDate(date);
-    } catch (e) {
-        console.log(e);
-    } 
+    const data = await axios.get(`https://api.github.com/users/${login}`);
+    console.log(data);
+    const date = data.data.created_at;
+    setRegDate(date);
   };
-
-  const dateDifference = formatDistance(
-    new Date(),
-    new Date(regDate),
-  );
 
   const onSubmit = async values => {
     await getRegDate(values.login);
-    setDateDiff(dateDifference);
   };
+    
+  
   
   return (
-    <div className="container">
-      <div className="a">{dateDiff}</div>
-        <Form
-          onSubmit={onSubmit}
-          render={({ handleSubmit }) => (
-          <form onSubmit={handleSubmit}>
-            <div className="form">
-              <label>Login</label>
-              <Field
-                name="login"
-                component="input"
-                type="text"
-                placeholder="Login"
-              />
-            </div>
-            <div className="button">
-              <button type="submit">
-                Submit
-              </button>      
-            </div>
-          </form>
-        )}
-      />
-    </div>
+    <div className={styles.container}>
+      <div className={styles.result}>{dateDiff || ''}</div>
+      <Form
+        className={styles.form}
+        onSubmit={onSubmit}
+        render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Login</label>
+            <Field
+              name="login"
+              component="input"
+              type="text"
+              placeholder="Login"
+            />
+          </div>
+          <div>
+            <button type="submit">
+              Submit
+            </button>      
+          </div>
+        </form>
+      )}
+    />
+  </div>
   );
 }
 
