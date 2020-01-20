89715e5db1a9210697673785aabe93eb3b2bbd9f
Тезяков Никита Андреевич <natezyakov@edu.hse.ru>
2020-01-20 17:14:40 +0500

    0.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import { formatDistance } from 'date-fns';
+import { Form, Field } from 'react-final-form';
+
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [regDate, setRegDate] = React.useState(0);
+  const [dateDiff, setDateDiff] = React.useState(0);
+
+  const getRegDate = async login => {
+    try {
+      const data = await axios.get(`https://api.github.com/users/${login}`);
+      const date = data.data.created_at;
+      setRegDate(date);
+    } catch (e) {
+        console.log(e);
+    } 
+  };
+
+  const dateDifference = formatDistance(
+    new Date(),
+    new Date(regDate),
+  );
+
+  const onSubmit = async values => {
+    await getRegDate(values.login);
+    setDateDiff(dateDifference);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="a">{dateDiff}</div>
+        <Form
+          onSubmit={onSubmit}
+          render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <div className="form">
+              <label>Login</label>
+              <Field
+                name="login"
+                component="input"
+                type="text"
+                placeholder="Login"
+              />
+            </div>
+            <div className="button">
+              <button type="submit">
+                Submit
+              </button>      
+            </div>
+          </form>
+        )}
+      />
     </div>
   );
 }
